08df0451087c1a94ffbd8b79fb04183013d500e6
Henrik Obsen <hto@e-train.dk>
2017-11-07 19:20:06 +0100

    First commit

---
+++ src/app/shopping-list/shopping-edit/spopping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spopping-edit',
+  templateUrl: './spopping-edit.component.html',
+  styleUrls: ['./spopping-edit.component.css']
+})
+export class SpoppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
